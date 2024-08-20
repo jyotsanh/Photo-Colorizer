f420c68ee9550f7e46bcc195505ce941b28ef642
mandip chaudhary <mandip.chaudhary2022@pcmgmt.edu.np>
2024-08-20 17:36:55 +0545

    about us page done

--- Frontend/src/components/AboutUs/about.jsx
+++ Frontend/src/components/AboutUs/about.jsx
@@ -1,12 +1,26 @@
-import "./about.css"
-function About(){
-    return(
-        <>
-            <div id="About">
-                <h1> About Us</h1>
-            </div>
-        </>
-    );
-}
+import React from 'react';
+import './about.css';
 
-export default About;
+const AboutUs = () => {
+  return (
+    <div className="about-container">
+      <header className="about-header">
+        <h1 className="about-title">About Us</h1>
+      </header>
+      <div className="about-content">
+        <p>
+          Temple vision is a place where the purpose of our project is for people to see the preserved work of past architecture of the city of temple Kathmandu. Our AI powered system decolorifies the image given by the user of Temple in black and white in color which gives the view to the past relic or memory.
+        </p>
+        <p>
+          Purpose of the project is to give the younger generation a memory visit to the past lane of History and to give the older generation a revisit of the memory lane.
+        </p>
+        <p>
+          Interns of scales, our project boundary has been set in Nepal's capital city Kathmandu (City of Temple).
+        </p>
+      </div>
+     
+    </div>
+  );
+};
+
+export default AboutUs;
--- Frontend/src/components/footer/footer.jsx
+++ Frontend/src/components/footer/footer.jsx
@@ -1,7 +1,12 @@
+import React from 'react';
+import './footer.css';
+
 function Footer(){
     return(
         <>
-            <footer>Footer</footer>
+            <footer className="about-footer">
+        <p>Copyright © 2024 Temple Vision. All rights reserved.</p>
+      </footer>
         </>
     );
 }
