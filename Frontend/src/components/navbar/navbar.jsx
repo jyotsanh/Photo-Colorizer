045453af3b3c90d2f436162537f7c9ecf5942ced
mandip chaudhary <mandip.chaudhary2022@pcmgmt.edu.np>
2024-10-10 15:45:52 +0545

    chnage css

--- Frontend/src/components/navbar/navbar.jsx
+++ Frontend/src/components/navbar/navbar.jsx
@@ -1,29 +1,22 @@
-import React from "react"
-import { useNavigate } from 'react-router-dom'
-import "./navbar.css"
-import Temple from '../../assets/loog.png'
-function NavBar(){
+import React from "react";
+import { useNavigate } from 'react-router-dom';
+import "./navbar.css";
+import Temple from '../../assets/loog.png';
+
+function NavBar() {
     const navigate = useNavigate();
     return (
-        <>
-            <div className="navbar">
-                <div className="logo-container">
-                    <img src={Temple}></img>
-                </div>
-
-                <div className="links-container">
-                    <p onClick={() => navigate("/")}> Home</p>
-                    <p onClick={() => navigate("/about")}> About Us</p>
-                    {/* <p onClick={() => navigate("/upload")}> Upload</p> */}
-                    <p onClick={() => navigate("/register")}> Sign Up</p>
-                    <p onClick={() => navigate("/login")}> Log-In</p>
-                    {/* <p onClick={() => navigate("/profile")}>profile</p>
-                    <p onClick={() => navigate("/logout")}>Logout</p> */}
-                    
-                </div>
+        <div className="navbar">
+            <div className="logo-container">
+                <img src={Temple} alt="Logo" />
             </div>
-        </>
-    )
+            <div className="links-container">
+                <p onClick={() => navigate("/")}>Home</p>
+                <p onClick={() => navigate("/about")}>About Us</p>
+                <p onClick={() => navigate("/login")}>Log In</p>
+            </div>
+        </div>
+    );
 }
 
 export default NavBar;
