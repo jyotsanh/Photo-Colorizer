0982221fe7c70d20a1b222a46a6f6fbd7448b03f
mandip chaudhary <mandip.chaudhary2022@pcmgmt.edu.np>
2024-10-15 10:07:01 +0545

    navbar resposive

--- Frontend/src/components/navbar/navbar.jsx
+++ Frontend/src/components/navbar/navbar.jsx
@@ -1,17 +1,31 @@
-
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import "./navbar.css";
 import Temple from '../../assets/loog.png';
 
 function NavBar() {
+    const [isOpen, setIsOpen] = useState(false);  // State for the hamburger menu
     const navigate = useNavigate();
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);  // Toggle the menu visibility
+    };
+
     return (
         <div className="navbar">
             <div className="logo-container">
                 <img src={Temple} alt="Logo" />
             </div>
-            <div className="links-container">
+
+            {/* Hamburger icon */}
+            <div className={`hamburger ${isOpen ? "open" : ""}`} onClick={toggleMenu}>
+                <span className="bar"></span>
+                <span className="bar"></span>
+                <span className="bar"></span>
+            </div>
+
+            {/* Links Container */}
+            <div className={`links-container ${isOpen ? "open" : ""}`}>
                 <p onClick={() => navigate("/")}>Home</p>
                 <p onClick={() => navigate("/about")}>About Us</p>
                 <p onClick={() => navigate("/login")}>Log In</p>
